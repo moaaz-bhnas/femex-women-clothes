373423a87da2d4e7d90091b12325b27dfa365e3e
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-04-05 17:36:26 +0400

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./index.scss', gulp.series('sass'));
+});
